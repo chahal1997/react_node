15ea5327332e0841111520103a3ef1a96d0b843f
chahal1997 <mesingh9719@gmail.com>
2024-03-05 17:19:42 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,51 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("server is ready")
+})
+
+const port = process.env.PORT || 46000;
+
+app.get('/jokes',(req,res)=>{
+    const jokes = [
+        {
+          id: 1,
+          title: "A Joke",
+          content: "This is a joke"
+        },
+        {
+          id: 2,
+          title: "Why don't skeletons fight each other?",
+          content: "They don't have the guts."
+        },
+        {
+          id: 3,
+          title: "What did the grape say when it got stepped on?",
+          content: "Nothing, it just let out a little wine."
+        },
+        {
+          id: 4,
+          title: "Why couldn't the bicycle stand up by itself?",
+          content: "It was two-tired."
+        },
+        {
+          id: 5,
+          title: "Why did the scarecrow win an award?",
+          content: "Because he was outstanding in his field."
+        },
+        {
+          id: 6,
+          title: "What's orange and sounds like a parrot?",
+          content: "A carrot!"
+        }
+      ];
+
+      res.send(jokes);
+      
+})
+
+app.listen(port,()=>{
+    console.log(`Server at http://localhost:${port}`);
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios';
+function App() {
+  const [jokes,setJokes] = useState([]);
+
+  useEffect(()=>{
+    axios.get('http://localhost:46000/jokes')
+    .then((response)=>{
+      setJokes(response.data)
+    }).catch((error)=>{
+      console.log(error);
+    })
+  })
+
+  return (
+    <>
+      <h1>FrontEnd</h1>
+      <div>
+      <p>
+        jokes
+      </p>
+      {
+        jokes.map((joke)=>{
+          <div key={joke.id}>
+            <h3>
+              {joke.title}
+            </h3>
+            <p>{joke.content}</p>
+          </div>
+        })
+      }
+      </div>
+
+    </>
+  )
+}
+
+export default App
